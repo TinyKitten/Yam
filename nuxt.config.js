6881c544c24716e114f160c0d4347d684b85be2f
TinyKitten <catwalk8205@gmail.com>
2018-08-17 18:23:56 +0900

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,34 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#2b90d9' },
+      { name: 'theme-color', content: '#ffffff' },
       {
         hid: 'description',
         name: 'description',
         content: 'Share to Mastodon, simply.',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'mask-icon', color: '#2b90d9', href: '/safari-pinned-tab.svg' },
+    ],
   },
   /*
   ** Customize the progress bar color
