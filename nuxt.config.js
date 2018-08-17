d477b966a03d4b9d60947d0a4d8c8cfb957305e5
TinyKitten <catwalk8205@gmail.com>
2018-08-17 18:10:43 +0900

    死ぬほど細かい調節

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'yam',
+    title: 'Yam',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,7 +18,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#2b90d9' },
   /*
   ** Build configuration
   */
